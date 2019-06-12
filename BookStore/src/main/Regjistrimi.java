d5dc8937eff52291d6b10309f2d5542b38df3cb5
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2019-06-12 21:47:58 +0200

    Update Regjistrimi.java

--- BookStore/src/main/Regjistrimi.java
+++ BookStore/src/main/Regjistrimi.java
@@ -31,3 +31,31 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+public class Regjistrimi extends Application{
+	//
+	ComboBox<String> languagesCbo = new ComboBox<>();
+
+	
+	private TextField tfEmri = new TextField();
+	private TextField tfMbiemri = new TextField();
+	private TextField tfEmriPerdoruesit = new TextField();
+	private TextField tfEmailAdresa = new TextField();
+	private PasswordField tfFjalekalimi = new PasswordField();
+	private PasswordField tfKonfirmoFjalekalimin = new PasswordField();
+	private TextField tfTelefoni = new TextField();
+	private TextField tfAdresa = new TextField();
+	private TextField tfQyteti = new TextField();
+	private ToggleGroup tgGjinia = new ToggleGroup();
+	//private RadioButton rbMashkull = new RadioButton("Mashkull");
+	private RadioButton rbMashkull = I18N.getRadioButton("Mashkull");
+	//private RadioButton rbFemer = new RadioButton("Femer");
+	private RadioButton rbFemer = I18N.getRadioButton("Femer");
+	
+	private static Stage secondStage;
+	private Kyqja kyqja = new Kyqja();
+	
+	private String css = "-fx-background-color:red;";
+
+	
+	@Override
+	public void start(Stage primaryStage) throws ClassNotFoundException {
